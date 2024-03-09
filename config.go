a001b46a883ec33631210167f584dc6fae4bcfc6
Parad0xPl <paradxpl@gmail.com>
2024-03-09 16:47:14 +0100

    feat: Add SSHFS params and extraction of them from config

--- config.go
+++ config.go
@@ -8,8 +8,29 @@ type EncConfig struct {
 	SSHFilePath        string
 	SSHMountPath       string
 	VeraCryptMountPath string
+	VeraCryptRepoPath  string
+}
+
+type SSHFSParams struct {
+	SSHPort         int
+	SSHAddress      string
+	SSHUser         string
+	SSHIdentityFile string
+	SSHFilePath     string
+	SSHMountPath    string
 }
 
 func GetConfig() EncConfig {
 	return EncConfig{}
+}
+
+func (c *EncConfig) ExtractSSHFSParams() SSHFSParams {
+	return SSHFSParams{
+		SSHPort:         c.SSHPort,
+		SSHAddress:      c.SSHAddress,
+		SSHUser:         c.SSHUser,
+		SSHIdentityFile: c.SSHIdentityFile,
+		SSHFilePath:     c.SSHFilePath,
+		SSHMountPath:    c.SSHMountPath,
+	}
 }
